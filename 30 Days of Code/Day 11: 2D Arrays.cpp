761f73609c1e7093251b33cb58136bb90ff1f470
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2020-06-30 08:19:27 +0600

    Update Day 11

--- 30 Days of Code/Day 11: 2D Arrays.cpp
+++ 30 Days of Code/Day 11: 2D Arrays.cpp
@@ -20,9 +20,9 @@ int main()
     int maxSum = INT_MIN;
     int sum = 0 ;
 
-    for (int row=0; row<4; row++)
+    for (int row = 0; row < 4; row++)
     {
-        for (int col=0; col<4; col++)
+        for (int col = 0; col < 4; col++)
         {
             sum = arr[row][col] + arr[row][col + 1] + arr[row][col + 2] +
                   arr[row + 1][col + 1] +
