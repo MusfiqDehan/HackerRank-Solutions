63afa941b4d6495b54cd614ac8e548c31412d0ae
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2020-06-30 08:16:20 +0600

    Add Day 11

---
+++ 30 Days of Code/Day 11 - 2D Arrays.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> arr(6);
+
+    for (int i = 0; i < 6; i++)
+    {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++)
+        {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int maxSum = INT_MIN;
+    int sum = 0 ;
+
+    for (int row=0; row<4; row++)
+    {
+        for (int col=0; col<4; col++)
+        {
+            sum = arr[row][col] + arr[row][col + 1] + arr[row][col + 2] +
+                  arr[row + 1][col + 1] +
+                  arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2];
+
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+            }
+        }        
+    }
+
+    cout << maxSum << endl;
+
+    return 0;
+}
