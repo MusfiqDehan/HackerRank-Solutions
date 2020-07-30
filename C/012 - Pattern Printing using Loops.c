668dd232ceb5d959874f5785e18371408ed32cb5
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2020-07-30 10:32:24 +0600

    Update Pattern Printing using Loops.c

--- C/012 - Pattern Printing using Loops.c
+++ C/012 - Pattern Printing using Loops.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-
     int n;
     scanf("%d", &n);
-    int len = n * 2 - 1;
+    
+    int len = (n * 2) - 1;
+    
     for (int i = 0; i < len; i++)
     {
         for (int j = 0; j < len; j++)
@@ -13,9 +14,12 @@ int main()
             int min = i < j ? i : j;
             min = min < len - i ? min : len - i - 1;
             min = min < len - j - 1 ? min : len - j - 1;
+            
             printf("%d ", n - min);
         }
+        
         printf("\n");
     }
+    
     return 0;
 }
