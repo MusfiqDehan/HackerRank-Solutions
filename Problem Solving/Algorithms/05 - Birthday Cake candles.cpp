5d8f56dc625e3a525de5381cf60caa1567b52cd5
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2020-08-28 14:26:36 +0600

    Update Problem-5

--- Problem Solving/Algorithms/05 - Birthday Cake candles.cpp
+++ Problem Solving/Algorithms/05 - Birthday Cake candles.cpp
@@ -7,10 +7,34 @@ int main()
     cin >> n;
     int arr[n];
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <n; i++)
     {
         cin >> arr[i];
     }
+
+    // int max = *max_element(arr, arr + n); //Using STL
+
+    int max = arr[0];
+    
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    // cout << max << endl;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (arr[i] == max)
+            count++;
+    }
+
+    cout << count << endl;
 
     return 0;
 }
