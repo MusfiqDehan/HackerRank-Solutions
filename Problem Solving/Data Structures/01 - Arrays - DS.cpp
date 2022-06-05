671ab72e248f11f472454a1d55af06b3883cace3
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-06-05 03:36:10 +0000

    update Problem Solving folder

---
+++ Problem Solving/Data Structures/01 - Arrays - DS.cpp
@@ -0,0 +1,26 @@
+//Array is the simplest and most used Data Structure
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
